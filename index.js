b94a7a0d7cae2851688d9dc8f3cb2effb1441a39
Clovis Santiago <sant_sivolc@hotmail.com>
2022-10-03 10:01:38 -0300

    primeiro commmit

---
+++ index.js
@@ -0,0 +1,9 @@
+alert('1º Para visualizar o projeto aperte f12 no teclado, 2º Vai abrir uma janela lateral no seu browser, 3º No menu click em Console, 4º aperte o ok, 5º veja o resultado no painel lateral e a mensagem na tela :)')
+for (let i = 0; i <= 10; i++){
+ console.log('O dobro do número', i ,'é',i * 2) 
+}
+
+
+
+
+
